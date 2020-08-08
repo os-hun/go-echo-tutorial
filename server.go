c9337f1154f3997bdf93edc3db896a2b5ec03479
Shun <shun12baseball@gmail.com>
2020-08-09 08:56:00 +0900

    new: go modulesを導入してechoをinstall add: echoでserverを立ち上げて Hello World!

--- server.go
+++ server.go
@@ -3,12 +3,19 @@ package main
 import (
 	"net/http"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	// echo instance
 	e := echo.New()
-	e.GET("/", func(c, echo.Context) error {
+	
+	// middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	
+	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":1323"))
 }
